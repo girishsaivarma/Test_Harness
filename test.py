a620f18445a66bd6648e5871826bb78d9ae75df5
girishsaivarma <151422355+girishsaivarma@users.noreply.github.com>
2023-11-19 02:16:53 -0500

    Update test.py

--- test.py
+++ test.py
@@ -64,7 +64,7 @@ class TestProgram(unittest.TestCase):
         self.run_test('gron', 'test', True)
 
         # Test 'ini2json' program without command line arguments
-        self.run_test('ini2json', 'test', True)
+        self.run_test('ini2json', 'test', False)
 
 if __name__ == '__main__':
     unittest.main()
